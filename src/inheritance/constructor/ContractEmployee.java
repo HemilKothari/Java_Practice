f545822979eb6bcda7ea4af09790e1fcaed51080
Hemil K <70153533+HemilKothari@users.noreply.github.com>
2025-06-25 13:09:39 +0530

    contract

---
+++ src/inheritance/constructor/ContractEmployee.java
@@ -0,0 +1,28 @@
+package inheritance.constructor;
+
+public class ContractEmployee extends Employee {
+    int ratePerHour;
+    float hoursWorked;
+
+    public ContractEmployee() {
+        System.out.println("Contract Employee");
+    }
+
+    public ContractEmployee(int id, String name, float hoursWorked, int ratePerHour) {
+        super(id, name);
+        this.hoursWorked = hoursWorked;
+        this.ratePerHour = ratePerHour;
+    }
+
+    @Override
+    void calculateSalary() {
+        salary = ratePerHour * hoursWorked;
+    }
+
+    void display() {
+        super.display();
+        System.out.println("Rate Per Hour: " + ratePerHour);
+        System.out.println("Hours Worked: " + hoursWorked);
+    }
+
+}
