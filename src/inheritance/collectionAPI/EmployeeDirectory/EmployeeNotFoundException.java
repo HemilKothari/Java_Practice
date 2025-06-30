896a179b427360fd824ab1d2b8ce87d6ef560479
Hemil K <70153533+HemilKothari@users.noreply.github.com>
2025-06-30 15:16:47 +0530

    Add files via upload

---
+++ src/inheritance/collectionAPI/EmployeeDirectory/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package inheritance.collectionAPI.EmployeeDirectory;
+
+public class EmployeeNotFoundException extends Exception{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
