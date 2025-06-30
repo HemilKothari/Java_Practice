fa5561a36b0429434711c7ae79b24619d6d662da
Hemil K <70153533+HemilKothari@users.noreply.github.com>
2025-06-30 09:16:12 +0530

    generics param

---
+++ src/generics/param/Exam.java
@@ -0,0 +1,24 @@
+package generics.param;
+
+public class Exam<C> {
+    private C code;
+
+    public C getCode() {
+        return code;
+    }
+
+    public void setCode(C code) {
+        this.code = code;
+    }
+
+    public Exam(C code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "Exam [code= " + code + ", type= " + code.getClass().getSimpleName() + "]";
+    }
+    
+    
+}
